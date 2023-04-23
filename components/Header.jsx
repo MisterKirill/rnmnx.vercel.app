9e82ffe0cc5516de000e36ce8c1fa21f5eb1828a
Kirill <misterkirill666@gmail.com>
2023-04-23 15:51:23 +0500

    ready

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,52 @@
 import Image from "next/image"
 
-import { FaTelegramPlane, FaSpotify } from "react-icons/fa"
-import { AiFillYoutube } from "react-icons/ai"
-
 import logo from "@/public/assets/logo.png"
+import vk from "@/public/assets/vk.png"
+import { useState } from "react"
 
 function Header() {
+    const [theme, setTheme] = useState('transparent')
+
+    function changeTheme(color) {
+        let el = document.getElementById('theme')
+
+        if(color) {
+            el.style.opacity = '1'
+            setTheme(`linear-gradient(${color}, transparent)`)
+        } else {
+            el.style.opacity = '0'
+        }
+    }
+
     return (
-        <header className="container px-[50px] c1:px-[250px] min-w-[100vw] absolute flex justify-between py-5 items-center">
-            <Image src={logo} sizes="60" width="60" alt="Logo" className="unselectable" />
-            <div className="flex gap-5">
-                <FaTelegramPlane className="text-white text-[40px]" />
-                <AiFillYoutube className="text-white text-[40px]" />
-                <FaSpotify className="text-white text-[40px]" />
-            </div>
-        </header>
+        <>
+            <div id="theme" className="absolute w-screen h-[500px] -z-10 transition-opacity duration-300 ease-in opacity-0" style={{ background: theme }}></div>
+            <header className="container px-[50px] c1:px-[250px] min-w-[100vw] absolute flex flex-col c3:flex-row gap-3 justify-between py-5 items-center">
+                <Image src={logo} sizes="60" width="60" priority alt="Logo" className="unselectable rotate-[10deg]" />
+                <div className="flex gap-5 items-center">
+                    <div className="flex flex-col items-center group">
+                        <a href="https://t.me/rnmnx" target="_blank" className="hover:mt-1"><i onMouseOver={() => changeTheme('#0088cc')} onMouseOut={() => changeTheme()} className="bi bi-telegram text-white text-[40px]" /></a>
+                        <span className="text-white absolute mt-[55px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none">Telegram</span>
+                    </div>
+                    <div className="flex flex-col items-center group">
+                        <a href="https://vk.com/rnmnx" target="_blank" className="hover:mt-1"><Image onMouseOver={() => changeTheme('#0077ff')} onMouseOut={() => changeTheme()} src={vk} width="40" alt="VK" /></a>
+                        <span className="text-white absolute mt-[45px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none">VK</span>
+                    </div>
+                    <div className="flex flex-col items-center group">
+                        <a href="https://www.youtube.com/channel/UCLtk7D27n95sQyNGN8mVBHQ" target="_blank" className="hover:mt-1"><i onMouseOver={() => changeTheme('#c3352e')} onMouseOut={() => changeTheme()} className="bi bi-music-note text-white text-[40px]" /></a>
+                        <span className="text-white absolute mt-[55px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none">YouTube Music</span>
+                    </div>
+                    <div className="flex flex-col items-center group">
+                        <a href="https://www.youtube.com/@rnmnxoff" target="_blank" className="hover:mt-1"><i onMouseOver={() => changeTheme('#ff0000')} onMouseOut={() => changeTheme()} className="bi bi-youtube text-white text-[40px]" /></a>
+                        <span className="text-white absolute mt-[55px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none">YouTube</span>
+                    </div>
+                    <div className="flex flex-col items-center group">
+                        <a href="https://open.spotify.com/artist/2LXZCBeH8vxw3x7Va9gba7" target="_blank" className="hover:mt-1"><i onMouseOver={() => changeTheme('#1db954')} onMouseOut={() => changeTheme()} className="bi bi-spotify text-white text-[40px]" /></a>
+                        <span className="text-white absolute mt-[55px] opacity-0 transition-opacity duration-300 group-hover:opacity-100 pointer-events-none">Spotify</span>
+                    </div>
+                </div>
+            </header>
+        </>
     )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
 
-import { FaCopy } from "react-icons/fa"
-import { BsDiscord } from "react-icons/bs"
-
 import Header from "@/components/Header"
 
 import avatar from "@/public/assets/avatar.jpg"
@@ -18,24 +15,39 @@ function Home() {
 		<>
 			<Head>
 				<title>RNMNX</title>
+				<meta property="og:title" content="RNMNX | Phonk продюсер" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://rnmnx.vercel.app" />
+				<meta property="og:image" content="http://my.site.com/images/thumb.png" />
+				<meta property="og:description" content="Московский фантазёр, запоминающийся загадочностью, строя события о любви, вдохновляясь музой. &quot;18.03 - и беды позади&quot;" />
+				<meta name="theme-color" content="#C5231E" />
 			</Head>
 			<Header />
 			<div className="flex justify-center">
-				<div className="flex flex-col c1:flex-row gap-16 mt-28 items-center">
+				<div className="flex flex-col c1:flex-row gap-16 mt-[180px] items-center">
 					<div className="flex">
-						<Image src={avatar} sizes="300" width="300" height="300" className="rounded-full max-w-[300px] max-h-[300px] unselectable" alt="Avatar" />
-						<Image src={note} sizes="60" width="60" height="50" alt="Note" className="max-w-[60px] max-h-[60px] unselectable absolute mt-[220px] ml-[220px] border-[#41110f] rounded-full border-[5px]" />
+						<Image src={avatar} sizes="300" width="300" height="300" priority className="rounded-full max-w-[300px] max-h-[300px] unselectable" alt="Avatar" />
+						<Image src={note} sizes="60" width="60" height="60" alt="Note" priority className="max-w-[60px] max-h-[60px] unselectable absolute mt-[220px] ml-[220px] border-[#41110f] rounded-full border-[5px]" />
 					</div>
 					<div className="flex flex-col items-center c1:items-start">
 						<div className="flex items-center">
-							<BsDiscord className="text-[30px] c3:text-[60px] text-white mr-2 c3:mr-5" />
+							<i className="bi bi-discord mt-1 text-[30px] c3:text-[60px] text-white mr-2 c3:mr-5" />
 							<span className="font-unbounded text-white text-[25px] c2:text-[60px] c3:text-[40px] mr-3">RNMNX#2528</span>
-							<FaCopy onClick={copyDiscord} className="text-[25px] text-white transition-opacity duration-500 opacity-80 hover:opacity-100 self-start c3:mt-3 cursor-pointer" />
+							<i onClick={copyDiscord} className="bi bi-clipboard-fill text-[25px] text-white transition-opacity duration-500 opacity-50 hover:opacity-100 self-start c3:mt-3 cursor-pointer" />
 						</div>
-						<span className="text-white text-[20px] c3:text-[30px]">Артист <span className="px-2 text-[25px] font-normal">&bull;</span> Phonk продюсер</span>
+						<span className="text-white font-semibold text-[20px] c3:text-[30px]">Артист <span className="px-2 text-[25px] font-normal">&bull;</span> Phonk продюсер</span>
 					</div>
 				</div>
 			</div>
+			<div className="flex justify-center mt-5">
+				<div className="p-5 bg-[#41110f] c2:rounded-xl w-screen c2:w-auto flex flex-col gap-3 items-center">
+					<span className="text-white font-extrabold text-[25px] flex items-center gap-3"><i className="bi bi-youtube text-[30px]" /> ПОСЛЕДНИЙ РЕЛИЗ</span>
+					<iframe width="560" className="w-[300px] c3:w-[500px] c3:h-[255px]" src="https://www.youtube.com/embed/HxY4pUVVExY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+				</div>
+			</div>
+			<span className="fixed bottom-0 left-0 mb-5 ml-5 text-[20px] font-bold text-white opacity-40 unselectable">
+				18/03
+			</span>
 		</>
 	)
 }
